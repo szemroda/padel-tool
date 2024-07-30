38714822e4f518eb7c94a6894fba74bddc45979d
Dawid Szemro <dawid.sz@tutanota.com>
2024-07-30 12:54:50 +0200

    Add optional Puppeteer browser args as ENV variable

--- modules/browser/index.js
+++ modules/browser/index.js
@@ -1,9 +1,13 @@
 const puppeteer = require('puppeteer');
-const { Logger } = require('../utils');
+const { Logger, Env } = require('../utils');
 
 const initializeBrowser = async () => {
     const browser = await puppeteer.launch({
         headless: true,
+        args: Env.get('PUPPETEER_BROWSER_ARGS', '')
+            .split(';')
+            .map(arg => arg.trim())
+            .filter(arg => arg),
     });
     const page = await browser.newPage();
     Logger.debug('Browser started');
