48dc873c8866e878d7360d4e1eb0eed672359c49
Dawid Szemro <dawid.sz@tutanota.com>
2024-08-01 11:32:56 +0200

    Refactor logger to include formatted timestamp in console messages

--- modules/utils/logger.js
+++ modules/utils/logger.js
@@ -1,8 +1,19 @@
 const Env = require('./env');
 
-const debug = text =>
-    Env.get('LOG_LEVEL', 'ERROR') === 'DEBUG' && console.debug(`[DEBUG] {${Date.now()}} - ${text}`);
+const getLogTimestamp = () => {
+    const date = new Date();
+    const isoDate = date.toISOString();
+    const offset = date.getTimezoneOffset() / 60;
+    const offsetString = offset < 0 ? `-${Math.abs(offset)}` : `+${offset}`;
 
-const error = text => console.error(`[ERROR] {${Date.now()}} - ${text}`);
+    return `${isoDate}${offsetString}`;
+};
+
+const debug =
+    Env.get('LOG_LEVEL', 'ERROR') === 'DEBUG'
+        ? text => console.debug(`[DEBUG] {${getLogTimestamp()}} - ${text}`)
+        : () => {};
+
+const error = text => console.error(`[ERROR] {${getLogTimestamp()}} - ${text}`);
 
 module.exports = { debug, error };
