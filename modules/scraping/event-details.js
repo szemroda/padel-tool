cd1d598c03eb3fd62e6b3c448a533182c352cb8c
Dawid Szemro <dawid.sz@tutanota.com>
2025-06-20 12:19:03 +0200

    Refactor event assignment check to avoid false positives and improve slot availability logic

--- modules/scraping/event-details.js
+++ modules/scraping/event-details.js
@@ -4,11 +4,12 @@ const getEventDetails = async (page, event) => {
     return await runInErrorContextAsync(
         async () => {
             await page.goto(event.link);
+            const assigned = await isUserAssignedToEvent(page);
 
             return {
                 ...event,
-                assigned: await isUserAssignedToEvent(page),
-                isSlotAvailable: await isSlotAvailable(page),
+                assigned,
+                isSlotAvailable: !assigned && (await isSlotAvailable(page)), // If user is assigned to event, we don't need to check if slot is available
             };
         },
         { event },
@@ -17,15 +18,30 @@ const getEventDetails = async (page, event) => {
 
 const isUserAssignedToEvent = async page => {
     const userName = Env.get('KLUBY_USER_NAME');
-    const assignedHandles = await page.$$('.list-group a');
+    const assignedHandles = await page.$$('table.table a');
     for (const handle of assignedHandles) {
         const text = await page.evaluate(element => element.innerText, handle);
-        if (text.includes(userName)) {
-            return true;
+
+        if (textIncludesUserFirstNameOrLastName(text, userName)) {
+            // Check the full name on the profile page to avoid false positives
+            const eventUrl = await page.url();
+            await handle.click();
+            await page.waitForNavigation({ waitUntil: 'networkidle0' });
+            const headingHandle = await page.$('h1');
+            const headingText = await page.evaluate(element => element.innerText, headingHandle);
+            await page.goto(eventUrl);
+
+            if (headingText.trim() === userName) {
+                return true;
+            }
         }
     }
 
     return false;
+};
+
+const textIncludesUserFirstNameOrLastName = (text, userName) => {
+    return userName.split(' ').some(namePart => text.trim().includes(namePart));
 };
 
 const isSlotAvailable = async page => {
