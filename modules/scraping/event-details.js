89af2972ff7469d6bb90d5e4b19689c25fc16dc3
Dawid Szemro <dawid.sz@tutanota.com>
2025-06-23 15:56:11 +0200

    Simplify event details data retrieval

--- modules/scraping/event-details.js
+++ modules/scraping/event-details.js
@@ -1,63 +1,46 @@
-const { Env, runInErrorContextAsync, Logger } = require('../utils');
+const { Env, runInErrorContextAsync } = require('../utils');
 
 const getEventDetails = async (page, event) => {
     return await runInErrorContextAsync(
         async () => {
-            await page.goto(event.link);
-            const assigned = await isUserAssignedToEvent(page);
+            const url = new URL(event.link);
+            const userName = Env.get('KLUBY_USER_NAME');
+            await page.goto(`${url.origin}${url.pathname}/uczestnicy`);
+            const badge = await page.$(`h4 span.badge`);
+            const [assigned, maxParticipants] = await page.evaluate(
+                x => x.innerText.split('/').map(x => parseInt(x.trim())),
+                badge,
+            );
+            const participants = await extractParticipantsNames(
+                page,
+                await page.$$(`.list-group a`),
+            );
+            const isUserAssigned = participants.some(x => x.includes(userName));
 
             return {
                 ...event,
-                assigned,
-                isSlotAvailable: !assigned && (await isSlotAvailable(page)), // If user is assigned to event, we don't need to check if slot is available
+                assigned: isUserAssigned,
+                isSlotAvailable: assigned < maxParticipants,
             };
         },
         { event },
     );
 };
 
-const isUserAssignedToEvent = async page => {
-    const userName = Env.get('KLUBY_USER_NAME');
-    const assignedHandles = await page.$$('table.table a');
-    for (const handle of assignedHandles) {
-        const text = await page.evaluate(element => element.innerText, handle);
+const extractParticipantsNames = async (page, elements) => {
+    const names = [];
 
-        if (textIncludesUserFirstNameOrLastName(text, userName)) {
-            // Check the full name on the profile page to avoid false positives
-            const eventUrl = await page.url();
-            await handle.click();
-            await page.waitForNavigation({ waitUntil: 'networkidle0' });
-            const headingHandle = await page.$('h1');
-            const headingText = await page.evaluate(element => element.innerText, headingHandle);
-            await page.goto(eventUrl);
+    for (const element of elements) {
+        const innerText = await page.evaluate(element => element.innerText, element);
+        const text = innerText?.trim() ?? '';
 
-            if (headingText.trim() === userName) {
-                return true;
-            }
+        // Consider only participant elements (starts with number).
+        if (/^[1-9]/.test(text)) {
+            names.push(text);
         }
     }
 
-    return false;
-};
-
-const textIncludesUserFirstNameOrLastName = (text, userName) => {
-    return userName.split(' ').some(namePart => text.trim().includes(namePart));
-};
-
-const isSlotAvailable = async page => {
-    const elementHandles = await page.$$('a.btn');
-    for (const handle of elementHandles) {
-        const text = await page.evaluate(element => element.innerText, handle);
-
-        if (text.toLowerCase().trim() === 'rezerwuj') {
-            const classes = await page.evaluate(element => Array.from(element.classList), handle);
-            const hasDisabledStatus = classes.some(x => x.includes('disabled'));
-            return !hasDisabledStatus;
-        }
-    }
-
-    Logger.error('No register button found!');
-    return false;
+    return names;
 };
 
 module.exports = {
